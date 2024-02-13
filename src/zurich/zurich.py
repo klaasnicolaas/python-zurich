b0ffb8cb8ebf5f04b66ad2b75cea1a9156d99ffd
Klaas Schoute <klaas_schoute@hotmail.com>
2024-02-13 17:11:20 +0100

    Update pylint and ruff configurations

--- src/zurich/zurich.py
+++ src/zurich/zurich.py
@@ -77,7 +77,7 @@ class ODPZurich:
                     ssl=True,
                 )
                 response.raise_for_status()
-        except asyncio.TimeoutError as exception:
+        except TimeoutError as exception:
             msg = "Timeout occurred while connecting to the Open Data Platform API."
             raise ODPZurichConnectionError(
                 msg,
