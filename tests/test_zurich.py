8a7ea40772a5aeb14a400f1b68c91be7e3f6db88
Klaas Schoute <klaas_schoute@hotmail.com>
2024-03-04 17:02:32 +0100

    Update code with Ruff format

--- tests/test_zurich.py
+++ tests/test_zurich.py
@@ -1,4 +1,5 @@
 """Basic tests for the Open Data Platform API of Zurich."""
+
 # pylint: disable=protected-access
 import asyncio
 from unittest.mock import patch
@@ -97,9 +98,12 @@ async def test_client_error() -> None:
     """Test request client error from the Open Data Platform API of Zurich."""
     async with ClientSession() as session:
         client = ODPZurich(session=session)
-        with patch.object(
-            session,
-            "request",
-            side_effect=ClientError,
-        ), pytest.raises(ODPZurichConnectionError):
+        with (
+            patch.object(
+                session,
+                "request",
+                side_effect=ClientError,
+            ),
+            pytest.raises(ODPZurichConnectionError),
+        ):
             assert await client._request("test")
